70583c067408e24bfd8330275674d4cfc933facf
Kishor.Ai <140939437+Kishor-Irnak@users.noreply.github.com>
2025-06-06 19:43:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
+});
+
+//functionality
+let [hours, minutes, seconds] = [0, 0, 0];
+let display = document.getElementById("display");
+let timer = null;
+let lapCount = 1;
+
+function updateDisplay() {
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  display.innerText = `${h}:${m}:${s}`;
+}
+
+function stopwatch() {
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+  updateDisplay();
+}
+
+function start() {
+  if (timer !== null) return;
+  timer = setInterval(stopwatch, 1000);
+}
+
+function pause() {
+  clearInterval(timer);
+  timer = null;
+}
+
+function reset() {
+  clearInterval(timer);
+  [hours, minutes, seconds] = [0, 0, 0];
+  updateDisplay();
+  timer = null;
+  document.getElementById("laps").innerHTML = "";
+  lapCount = 1;
+}
+
+function lap() {
+  const lapTime = display.innerText;
+  const li = document.createElement("li");
+  li.innerText = `Lap ${lapCount++}: ${lapTime}`;
+  document.getElementById("laps").appendChild(li);
+}
